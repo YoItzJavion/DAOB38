0517b45ef13ed089451121cbcac13a4e05b6ee40
Javion <90114927+YoItzJavion@users.noreply.github.com>
2022-03-14 16:51:52 -0400

    Update and rename untitled.js to auto-role.js

--- DAOB38 Files/auto-role.js
+++ DAOB38 Files/auto-role.js
@@ -1,4 +1,4 @@
-// A simple auto role command for your server!
+
 const lib = require('lib')({token: process.env.STDLIB_SECRET_TOKEN});
 await lib.discord.guilds['@0.1.0'].members.roles.update({
   role_id: `941806937627037`, // Put the ID of your member role here!
